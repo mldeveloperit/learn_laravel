3f1accf6ad3c15463fb470dd6f400228b7a418d9
lashgari <mlashgari.it@gmail.com>
2019-06-06 07:21:37 +0430

    work on routing

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -79,7 +79,7 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Hello {{$name}} {{$family}}
+            Hello Mr {{$name}} {{$family}}
         </div>
     </div>
 </div>
